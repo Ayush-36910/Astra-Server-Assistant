e470fd5457e85b2f2f95214aecdf0eebc58278a0
Ayushman <ayushmansharma4837@gmail.com>
2026-09-25 15:02:53 +0530

    Update astra_api.py

--- astra_api.py
+++ astra_api.py
@@ -9,7 +9,7 @@ from google.genai import types
 from groq import Groq
 from dotenv import load_dotenv
 
-# Keys load karna
+# load keys
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
@@ -18,7 +18,7 @@ if not GEMINI_API_KEY or not GROQ_API_KEY:
     print("Error: API Keys missing! Check .env file.")
     exit()
 
-# Clients initialize karna
+# initialize clints
 gemini_client = genai.Client(api_key=GEMINI_API_KEY)
 groq_client = Groq(api_key=GROQ_API_KEY)
 
@@ -45,17 +45,17 @@ async def generate_audio(text, voice="hi-IN-SwaraNeural", output_file="reply.mp3
     communicate = edge_tts.Communicate(text, voice)
     await communicate.save(output_file)
 
-# Naya Endpoint jo Audio File receive karega
+# endpoint which will recieve an audio file
 @app.post("/ask-voice", response_class=FileResponse)
 async def ask_voice_endpoint(audio_file: UploadFile = File(...)):
     print("\n[ESP32 -> Server] Ek nayi audio file aayi hai...")
     
-    # 1. Upload hui audio file ko temporarily save karna
+    # 1. save temparpry audio file 
     temp_input_path = "temp_input.wav"
     with open(temp_input_path, "wb") as f:
         f.write(await audio_file.read())
         
-    # 2. GROQ STT (Aawaz ko text mein badalna)
+    # 2. GROQ STT (sound into text)
     print("[Groq] Aawaz ko text mein convert kar raha hai...")
     with open(temp_input_path, "rb") as file:
         transcription = groq_client.audio.transcriptions.create(
@@ -66,7 +66,7 @@ async def ask_voice_endpoint(audio_file: UploadFile = File(...)):
     user_text = transcription.text
     print(f"[User Ka Sawaal]: {user_text}")
     
-    # 3. GEMINI LLM (Sawaal ka jawab sochna with SMART RETRY)
+    # 3. GEMINI LLM (question's answers)
     print("[Gemini] Jawab soch raha hai...")
     reply_text = "माफ़ करना, सर्वर अभी व्यस्त है।" # Default error message
     
@@ -74,7 +74,7 @@ async def ask_voice_endpoint(audio_file: UploadFile = File(...)):
         try:
             response = chat.send_message(user_text)
             reply_text = response.text
-            break # Agar success ho gaya toh loop tod do
+            break # if status = true then break the loop
         except Exception as e:
             if "503" in str(e):
                 print(f"[Server thoda busy hai... Retrying {attempt+1}/3]")
@@ -85,9 +85,9 @@ async def ask_voice_endpoint(audio_file: UploadFile = File(...)):
                 
     print(f"[Astra Ka Jawab]: {reply_text}")
     
-    # 4. EDGE-TTS (Jawab ko aawaz mein badalna)
+    # 4. EDGE-TTS (text to voice)
     output_audio_path = "reply.mp3"
     await generate_audio(reply_text, output_file=output_audio_path)
     
-    # 5. Audio file ko wapas bhej dena
+    # 5. send the audio file
     return FileResponse(output_audio_path, media_type="audio/mpeg")
